8e3edc3fa4c395db02ded384f013cf214e61863e
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-26 23:25:43 +0000

    Updated code to accumulate total across different objects with same company name and linted

--- sales_tax/sales.js
+++ sales_tax/sales.js
@@ -23,12 +23,45 @@ const companySalesData = [
  
 ];
 
-
+const calculateTotalSales = function(salesArray) {
+  let total = 0;
+  for (const sales of salesArray) {
+    if (isNaN(sales) === false) {
+      total += parseInt(sales);
+    }
+  }
+  return total;
+};
 
 const calculateSalesTax = function(salesData, taxRates) {
+  const totalSalesAndTaxes = new Object;
+  let totalSales = 0;
+  let totalTaxes = 0;
+
   // Iterate through each object within company sales data
+  for (const company of salesData) {
+    // Store values into keys for the company
     // calculate the total sales for the company
+    totalSales = calculateTotalSales(company.sales);
     // Determine the tax rate for the province for the company
     // Multiply total sales by tax rate for company
-    // Store values into keys for the company
-}
+    totalTaxes = totalSales * taxRates[company.province];
+    // Check whether the current company already exists in the object
+    if (!totalSalesAndTaxes[company.name]) {
+      // Store total sales into a new object for the total sales object per company
+      totalSalesAndTaxes[company.name] = {
+        "totalSales": totalSales,
+        "totalTaxes": totalTaxes
+      };
+      // If current company already has been recorded. Add new values to the total
+    } else {
+      totalSalesAndTaxes[company.name].totalSales += totalSales;
+      totalSalesAndTaxes[company.name].totalTaxes += totalTaxes;
+    }
+  }
+  return totalSalesAndTaxes;
+};
+
+
+
+console.log(calculateSalesTax(companySalesData, salesTaxRates));
